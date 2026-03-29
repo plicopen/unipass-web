a7226cee6e1b89632ccfb72a28ed8764fcb0b12e
plicopen <floit0805@gmail.com>
2026-03-29 14:01:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ API_KEY  = os.environ.get('UNIPASS_API_KEY', '')
 PASSWORD = os.environ.get('APP_PASSWORD', 'unipass1234')
 API_BASE = 'https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo'
 
-import certifi
-SSL_CTX = ssl.create_default_context(cafile=certifi.where())
+SSL_CTX = ssl.create_default_context()
 SSL_CTX.check_hostname = False
 SSL_CTX.verify_mode = ssl.CERT_NONE
 
